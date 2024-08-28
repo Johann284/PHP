eec3988bdf0ab0b354e1933e531649b24502cbfe
Johann284 <160616489+Johann284@users.noreply.github.com>
2024-08-28 11:48:42 -0300

    Update Atividade_05.php

--- Aula_03/Atividade_05.php
+++ Aula_03/Atividade_05.php
@@ -2,48 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name= "viewport" content="width=device-width, initial-scale= 1.0">
-    <style type="text/css">
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-
-        form {
-            max-width: 400px;
-            margin: 0 auto;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px; 
-
-        }
-
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-
-        input[type="number"] {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 10px;
-            border: 1px solid #ccc;
-            border-radius: 
-        3px;
-        }
-
-        button[type="submit"] {
-            background-color: green;
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 3px;
-        }
-
-        button[type="submit"]:hover {
-            background-color: yellowgreen;
-            transition: 2s;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
     <title>Informa Primo</title> 
 </head>
 <body>
