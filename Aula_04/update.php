e2dacc9cbbeffdf857ab6451e35f3aed54e4ef55
Johann284 <160616489+Johann284@users.noreply.github.com>
2024-09-02 11:54:46 -0300

    Create update.php

---
+++ Aula_04/update.php
@@ -0,0 +1,45 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UPDATE</title>
+</head>
+<body>
+    
+<form method="POST" action="update.php">
+    <label for="name">Nome: </label>
+        <input type="text" name="name" require>
+    <label for="upd_name">Novo Nome: </label>
+        <input type="text" name="upd_name" require>
+    <br>
+    <label for="email">Email: </label>
+        <input type="text" name="email" require>
+    <label for="upd_email">Novo Email: </label>
+        <input type="text" name="upd_email" require>
+    <input type="submit" value="Alterar">
+</form>
+
+    
+<a href="create.php"><button>Voltar.</button></a>
+</body>
+</html>
+
+<?php
+include "bd.php";
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $name = $_POST['name'];
+        $email = $POST['email'];
+        $updName = $_POST['upd_name'];
+        $updEmail = $_POST['upd_email'];
+        $sql = "UPDATE user SET user name = $updName, email = $updEmail WHERE name = $name";
+
+        if ($conn -> query($sql) === true){
+            echo "Novo resgistro criado com sucesso!";
+        }else{
+            echo "Erro" . $sql . "<br>" . $conn -> error;
+        }
+    }
+$conn -> close();
+
+?>
