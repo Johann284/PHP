0cf6150b321cc86df8e7d732cb80ec7d70f06ef1
Johann284 <johann_ruth@estudante.sesisenai.org.br>
2024-09-16 11:35:51 -0300

    teste

---
+++ crud_na_mesma_tela/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+
+?>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tudo na mesma tela</title>
+</head>
+<body>
+    
+</body>
+<script src="javaScript.js"></script>
+</html>
